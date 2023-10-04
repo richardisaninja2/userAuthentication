3359ac6b07794848e16cd516f09086eb905462ba
richard murrray <murray571a@gmail.com>
2023-10-04 09:37:53 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+import { useState } from 'react';
+import Login from './components/Login/Login';
+import useToken from './components/App/useToken';
+
 
 function App() {
+  const { token, setToken } = useToken();
+  // const token = getToken();
+
+  if(!token){
+    return <Login setToken={setToken}/>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/dashboard" element={<Dashboard/>}/>
+  
+          <Route exact path="/preferences" element={<Preferences/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
